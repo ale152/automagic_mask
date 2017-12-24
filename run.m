020ced8f6837e74fd98406a0284f4b1b9ddf6538
Alessandro Masullo <am14795@bristol.ac.uk>
2017-12-24 13:34:28 +0000

    Simplify run function

--- run.m
+++ run.m
@@ -1,23 +1,47 @@
+% Simply run this script
+
 % Image path
-imdir = '\my_image_path\';
-imname = 'A_%05d_a.tif';
-range = [1 600];
+FilterSpec = '*.tif;*.tiff;*.jpg;*.png;*.bmp;*.gif';
+DialogTitle = 'Select multiple images to generate mask...';
+[FileName,PathName,FilterIndex] = uigetfile(FilterSpec,DialogTitle,'MultiSelect','on');
 
 % Number of images
-Nim = range(2)-range(1)+1;
+Nim = numel(FileName);
 
 % Read a sample image
-sample = imread(fullfile(imdir,sprintf(imname,range(1))));
+sample = imread(fullfile(PathName,FileName{1}));
 
 % Load the images
 rangev = range(1):range(2);
 imdb = zeros(size(sample,1),size(sample,2),Nim,'like',sample);
-c = 0; % Just for display
-for ri = 1:Nim
-    fprintf('%s',repmat(char(8),1,c))
-    c = fprintf('Loading image %d of %d (%s)\n',ri,Nim,sprintf(imname,ri));
-    imdb(:,:,ri) = imread(fullfile(imdir,sprintf(imname,rangev(ri))));
+good_im = 0;
+tic
+for ii = 1:Nim
+    if toc > 1
+        fprintf('Loading image %d of %d (%s)\n',ii,Nim,FileName{ii}); tic
+    end
+    % Try to read the image
+    try
+        img = imread(fullfile(PathName,FileName{ii}));
+        % Convert rgb to gray
+        if size(img,3) > 1
+            img = rgb2gray(img);
+        end
+        imdb(:,:,ii) = img;
+        good_im = good_im+1;
+    catch me
+        % Skip image if something goes wrong
+        fprintf(2,'Skipping image %s because of %s\n',FileName{ii},me.message);
+        continue
+    end
 end
 
+if good_im < 2
+    error('You must select at least 2 images to run the script')
+end
+
+if good_im < 50
+    warning('The number of images selected might be insufficient for the script to work')
+end
 % Generate the mask showing the debug information
 [mask,p] = mask_detect(imdb,1);
